1ed83cb3a814ecbfd579bc33b516eb4b044fc94f
Ruiqi Mao <ruiqim@gmail.com>
2015-01-02 17:06:40 -0800

    tabs

---
+++ js/main.js
@@ -0,0 +1,13 @@
+function switchTab() {
+	$(".panel").fadeOut({'duration':200,'queue':false});
+	$("#"+$(".selected").data("panel")).fadeIn({'duration':200,'queue':false});
+}
+
+$(".tab").click(function() {
+	if($(this).hasClass("selected")) return;
+	$(".tab").removeClass("selected");
+	$(this).addClass("selected");
+	switchTab();
+});
+
+switchTab();
