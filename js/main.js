8c3ac80a93f85006625828510dd00e480a763b1f
Ruiqi Mao <ruiqim@gmail.com>
2015-01-02 22:12:18 -0800

    component drive functionality

--- js/main.js
+++ js/main.js
@@ -1,3 +1,5 @@
+var options = {'motor':['port1','port2','port3','port4','port5','port6','port7','port8','port9','port10']};
+
 var components = [];
 var selectedComponent = 0;
 
@@ -45,7 +47,12 @@ function updateComponentList() {
 $(".new-component").click(function() {
 	var i = 1;
 	while(getComponent("Untitled Component "+i)) i ++;
-	components.push({'name':'Untitled Component '+i,'type':0});
+	components.push({'name':'Untitled Component '+i,
+					'type':0,
+					'drive-motors-left':[],
+					'drive-motors-right':[],
+					'drive-encoder-left':0,
+					'drive-encoder-right':0});
 	selectedComponent = components.length-1;
 	updateComponentList();
 });
@@ -67,6 +74,21 @@ $(".save-component").click(function() {
 	}
 	components[selectedComponent]['name'] = newName;
 	components[selectedComponent]['type'] = $(".config-component-type.selected").index();
+	var paneIndex = $(".config-component-type.selected").index();
+	if(paneIndex == 0) {
+		var motorsLeft = [];
+		var motorsRight = [];
+		$("#config-component-drive-motor-left").children().first().children().each(function() {
+			motorsLeft.push($(this).data("index"));
+		});
+		$("#config-component-drive-motor-right").children().first().children().each(function() {
+			motorsRight.push($(this).data("index"));
+		});
+		components[selectedComponent]['drive-motors-left'] = motorsLeft;
+		components[selectedComponent]['drive-motors-right'] = motorsRight;
+		components[selectedComponent]['drive-encoder-left'] = $("#config-component-drive-encoder-left").val();
+		components[selectedComponent]['drive-encoder-right'] = $("#config-component-drive-encoder-right").val();
+	}
 	$(".config-component.selected").text(newName);
 	showMessage("Saved!");
 });
@@ -74,6 +96,7 @@ $(".save-component").click(function() {
 $(".config-component-type").click(function() {
 	$(".config-component-type").removeClass("selected");
 	$(this).addClass("selected");
+	displayConfigComponentPane();
 });
 
 function displayComponent() {
@@ -88,8 +111,52 @@ function displayComponent() {
 		$(".config-component-name").val(component['name']);
 		$(".config-component-type").removeClass("selected");
 		$(".config-component-type:nth-child("+(component['type']+1)+")").addClass("selected");
+		displayConfigComponentPane();
 	}
 }
+
+function displayConfigComponentPane() {
+	var component = components[selectedComponent];
+	var paneIndex = $(".config-component-type.selected").index();
+	$(".config-component-pane").hide();
+	$(".config-component-pane."+paneIndex).show();
+	if(paneIndex == 0) {
+		$("#config-component-drive-motor-left").children().first().empty();
+		$("#config-component-drive-motor-right").children().first().empty();
+		for(var i = 0; i < component['drive-motors-left'].length; i ++) {
+			addOptionToBox($("#config-component-drive-motor-left"),component['drive-motors-left'][i]);
+		}
+		for(var i = 0; i < component['drive-motors-right'].length; i ++) {
+			addOptionToBox($("#config-component-drive-motor-right"),component['drive-motors-right'][i]);
+		}
+		$("#config-component-drive-encoder-left").val(component['drive-encoder-left']);
+		$("#config-component-drive-encoder-right").val(component['drive-encoder-right']);
+	}
+}
+
+function addOptionToBox(optionsGroup,index) {
+	var optionsBox = optionsGroup.children().first();
+	var optionType = optionsGroup.data("type");
+	var newOption =  $('<span class="option" data-type="'+optionType+'" data-index="'+index+'"><span>'+options[optionType][index]+'</span></span>');
+	var newOptionClose = $('<img class="option-close" src="images/icons/cancel.png" />');
+	newOptionClose.click(function() {
+		$(this).parent().remove();
+	});
+	newOption.click(function() {
+		var subOptions = options[$(this).data("type")];
+		var index = $(this).data("index");
+		index ++;
+		if(index >= subOptions.length) index = 0;
+		$(this).data("index",index);
+		$(this).children().first().text(subOptions[index]);
+	});
+	newOption.append(newOptionClose);
+	optionsBox.append(newOption);
+}
+
+$(".options-button").click(function() {
+	addOptionToBox($(this).parent(),0);
+});
 
 $(window).bind('keydown.ctrl_s keydown.meta_s', function(event) {
 	if(event.keyCode == 83) {
